13a8a9ba8b6da0b695d9ef1bd118841c197c7d7f
Mpoked <pokorny_marek@oauh.cz>
2025-02-07 12:14:22 +0100

    asdad

---
+++ src/LevePrave.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+
+public class LevePrave extends JFrame{
+    private JButton btLeva;
+    private JButton btPrava;
+    private JPanel pnMain;
+
+    public LevePrave() {
+        initComponent();
+    }
+
+    private void initComponent() {
+        setContentPane(pnMain);
+        setTitle("Hello GUI");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        pack();
+        btLeva.addActionListener(e -> leva());
+        btPrava.addActionListener(e -> prava());
+    }
+
+    private void leva() {
+        JOptionPane.showMessageDialog(this, "levá");
+    }
+    private void prava() {
+        JOptionPane.showMessageDialog(this, "pravá");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        LevePrave levaprave = new LevePrave();
+        levaprave.setVisible(true);
+    }
+}
